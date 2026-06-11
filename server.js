259dfb82a29beb895aa360b19382c6e944dea18f
titarenkoillia97-byte <titarenkoillia97@gmail.com>
2026-06-11 23:19:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ initDB();
 
 // Главная страница
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Получить текущее количество лайков
